8a628ba8b6efae1712e2ae35ccb4f96342bd19e5
Gyanesh Rao <gyaneshrao28@gmail.com>
2024-06-01 03:45:03 +0530

    v1.1.1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+   
+  },
   server:{
     proxy:{
       '/api': 'https://course-app-api-beta.vercel.app'
